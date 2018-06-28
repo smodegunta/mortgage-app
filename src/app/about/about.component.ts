854a493e3eb2ed74c830bc31ea04c146be0a605c
deepankm <deepan.kumar@brimmatech.com>
2018-06-28 22:44:56 +0530

    draft

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 import { environment } from '@env/environment';
 
-const URL = 'path_to_api';
+const URL = 'http://573ce40c.ngrok.io/borrower/loan/submitLoan';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
+ api = URL;
 }
