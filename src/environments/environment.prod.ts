f33cf0df76917706986e5cb31afc919b4f3f16ae
Kirupakaran <kirupakaran@brimmatech.com>
2018-08-17 14:03:53 +0530

    Update env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from '@env/.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://222a2723.ngrok.io/',
+  serverUrl: 'http://localhost:9099/',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
