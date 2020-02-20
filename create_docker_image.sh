87619cfd32d2fd42dae0d85a959860eb90d77eff
Srikanth Modegunta <srikanth@brimmatech.com>
2020-02-20 20:55:11 +0530

    removed node modules

--- create_docker_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ng build --prod
-#ng build --prod --base-href /ui/ --deploy-url /ui/
-docker build -t 961085435907.dkr.ecr.us-east-1.amazonaws.com/brimma/mortgageui .
-#docker build -t brimma/mortgageui .
-docker push 961085435907.dkr.ecr.us-east-1.amazonaws.com/brimma/mortgageui:latest
